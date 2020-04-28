180d9cbc934f77d6b699214e715a7bbe6fcb91a9
James Jackson-South <james_south@hotmail.com>
2020-04-28 14:34:30 +0100

    Disable throwing benchmark

--- tests/ZlibStream.Benchmarks/ZlibDeflateBenchmark.cs
+++ tests/ZlibStream.Benchmarks/ZlibDeflateBenchmark.cs
@@ -14,13 +14,11 @@ namespace ZlibStream.Benchmarks
     {
         private byte[] data;
 
-        [Params(1024, 2048, 4096)]
-        public int Count { get; set; }
-
         [GlobalSetup]
         public void SetUp()
         {
-            this.data = GetBuffer(this.Count);
+            // Equivalent to 1MP Rgba image
+            this.data = GetBuffer(1000 * 1000 * 4);
         }
 
         [Benchmark]
@@ -54,22 +52,23 @@ namespace ZlibStream.Benchmarks
             }
         }
 
-        [Benchmark(Description = "ZLibManaged")]
-        public long ZlibManagedDeflate()
-        {
-            using (var output = new MemoryStream())
-            {
-                // Defaults to compression -1, buffer 512.
-                using (var deflate = new Elskom.Generic.Libs.ZOutputStream(output, (int)ZlibCompressionLevel.ZDEFAULTCOMPRESSION))
-                {
-                    deflate.Write(this.data, 0, this.data.Length);
-                }
-            }
-
-            // TODO: Upgrade when Dispose() fix is merged.
-            return this.data.Length;
-        }
-
+        // TODO: Enable when BMDN stops throwing.
+        // [Benchmark(Description = "ZLibManaged")]
+        // public long ZlibManagedDeflate()
+        // {
+        //    using (var output = new MemoryStream())
+        //    {
+        //        // Defaults to compression -1, buffer 512.
+        //        using (var deflate = new Elskom.Generic.Libs.ZOutputStream(output, (int)ZlibCompressionLevel.ZDEFAULTCOMPRESSION))
+        //        {
+        //            deflate.Write(this.data, 0, this.data.Length);
+        //        }
+        //    }
+        //
+        // // TODO: Upgrade when Dispose() fix is merged.
+        //    return this.data.Length;
+        // }
+        //
         private static byte[] GetBuffer(int length)
         {
             var data = new byte[length];
